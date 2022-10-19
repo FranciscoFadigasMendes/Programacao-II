9382c6c018a629fd7551a2e5a209418f3673fe33
FranciscoFadigasMendes <79844404+FranciscoFadigasMendes@users.noreply.github.com>
2022-10-19 13:42:19 +0100

    Update Aplicação.java

--- Folha 2/Ex.4/Aplicação.java
+++ Folha 2/Ex.4/Aplicação.java
@@ -2,59 +2,66 @@ package com.mycompany.aplicacao;
 import java.util.Scanner;
 
 public class Aplicacao {
-Produtos p[];
+static Produtos p[];
+static int num_prod[];
+
 
     public static void main(String[] args) {
-        int product_counter = 0,ans = 1;
+        int ans = 1;
         
         //LAYOUT DO MENU
         System.out.println("\nBEM-VINDO AO GESTOR DE PRODUTOS:\n");
         System.out.print("->Adicionar um Produto (1)");
-        System.out.println("\t\t->Consultar um Produto (2)");
-        System.out.print("->Listar os Produtos (3)");
-        System.out.println("\t\t->Alterar Nome de um Produto (4)");
-        System.out.println("->Alterar Stock de um Produto (5)");
+        System.out.println("\t\t->Eliminar um Produto (2)");
+        System.out.print("->Consultar um Produto (3)");
+        System.out.println("\t\t->Listar os Produtos (4)");
+        System.out.print("->Alterar Nome de um Produto (5)");
+        System.out.println("\t->Alterar Stock de um Produto (6)");
         System.out.print("---------------------------------------------------------------------");
+        
         Scanner opcao = new Scanner(System.in);
         System.out.print("\n Indique a sua opção: ");
-        char opc = opcao.next().charAt(0);
+        int opc = opcao.nextInt();
+   
         System.out.print("\n");
         //----------------------------------------------------------------
 
-        do{
             switch(opc) {
-                case '1':
-                    adicionar_produto(product_counter);
-                    break;
-                case '2':
+                case 1:
+                    adicionar_produto();
+                    main(args);
+                case 2:
+                    eliminar_produto();
+                    main(args);
+                case 3:
                      consultar_produto();
-                    break;
-                case '3':
-                     listar_produto(product_counter,p[]);
-                    break;
-                case '4':
+                    main(args);
+                case 4:
+                     listar_produto();
+                    main(args);
+                case 5:
                      alterar_nome_produto();
-                    break;
-                case '5':
+                    main(args);
+                case 6:
                      alterar_stock_produto();
-                    break;
+                    main(args);
                  
                 default:
                         ans = 0;}
-            }while(ans == 0);
-   
+            
     }    
   //--------------------------------------------------------------------------      
-    public static void adicionar_produto(int product_counter){
+    public static void adicionar_produto(){
         System.out.print("\nFunção: adicionar_produto\n");
+        
         
         Scanner answer = new Scanner(System.in);
         System.out.print("Quantos produtos pretende criar? --> ");
-        int a = answer.nextInt();
+        num_prod[0] = answer.nextInt();
+         
+        Produtos p[] = new Produtos[num_prod[0]];
         
-        p[] = new Produtos[a];
-        
-        for(int i=0;i<a;i++){
+        for(int i=0;i<num_prod[0];i++){
         //PEDE O ID
         Scanner p_1 = new Scanner(System.in);
         System.out.print("Insira o ID do Produto " + (i+1) + ":");
@@ -72,10 +79,10 @@ Produtos p[];
         
         //CRIA OBJETO DO CONSTRUTOR COM OS ATRIBUTOS PEDIDOS ACIMA 
         p[i] = new Produtos(p_id,p_nome,p_stock);
-        
-        product_counter++;
+       
         System.out.print("\n\n");
         }
+    
         
     }
 //--------------------------------------------------------------------------  
@@ -84,21 +91,22 @@ Produtos p[];
       
     }
 //--------------------------------------------------------------------------  
-    public static void consultar_produto(int product_counter){
+    public static void consultar_produto(){
       System.out.print("\nFunção: consultar_produto\n"); 
                   
+
+    }
+//-------------------------------------------------------------------------- 
+     public static void listar_produto(){
+      System.out.print("\nFunção: listar_produto\n");
+      
       //MOSTRA VALORES GUARDADOS
-        for(int i=0;i<product_counter;i++){       
+        for(int i=0;i<num_prod[0];i++){       
             System.out.println("ID Nº" + (i+1) + "-->"+ p[i].getId());
             System.out.println("Nome Nº" + (i+1) + "-->"+ p[i].getNome());
             System.out.println("Stock Nº" + (i+1) + "-->"+ p[i].getStock());
             System.out.print("\n");
         }
-    }
-//-------------------------------------------------------------------------- 
-     public static void listar_produto(){
-      System.out.print("\nFunção: listar_produto\n");
-
       
     }
 //-------------------------------------------------------------------------- 
