1b4d2fbc479909680bfa9f67d9d209dc415fd82a
FUNKIOR <105812771+FUNKIOR@users.noreply.github.com>
2022-11-01 17:06:31 +0000

    Update Motorista.java

--- PROJETO/Motorista.java
+++ PROJETO/Motorista.java
@@ -1,6 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package gestãotransportes;
 
-
 public class Motorista {
+    
+    //ATRIBUTOS
+    private int NIF, Telefone;
+    private String Nome, Morada, Data, Empresa, Habilitação;
+
+    //CONSTRUTOR
+    public  Motorista(int NIF, String Nome, String  Morada, int Telefone, String Habilitação, String Empresa, String Data){
+
+        this.NIF = NIF;
+        this.Nome = Nome;
+        this.Morada = Morada;
+        this.Telefone = Telefone;
+        this.Habilitação = Habilitação;
+        this.Empresa = Empresa;
+        this.Data = Data;
+    }
+
+    /**
+     * @return the NIF
+     */
+    public int getNIF() {
+        return NIF;
+    }
+
+    /**
+     * @return the Telefone
+     */
+    public int getTelefone() {
+        return Telefone;
+    }
+
+    /**
+     * @return the Habilitação
+     */
+    public String getHabilitação() {
+        return Habilitação;
+    }
+
+    /**
+     * @return the Nome
+     */
+    public String getNome() {
+        return Nome;
+    }
+
+    /**
+     * @return the Morada
+     */
+    public String getMorada() {
+        return Morada;
+    }
+
+    /**
+     * @return the Data
+     */
+    public String getData() {
+        return Data;
+    }
+
+    /**
+     * @return the Empresa
+     */
+    public String getEmpresa() {
+        return Empresa;
+    }
     
 }
