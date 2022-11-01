bc368985d2415605b98bfe1985d84af7ecb45dd1
FranciscoFadigasMendes <79844404+FranciscoFadigasMendes@users.noreply.github.com>
2022-11-01 13:59:19 +0000

    Update GestãoTransportes.java

--- PROJETO/GestãoTransportes.java
+++ PROJETO/GestãoTransportes.java
@@ -5,42 +5,39 @@ public class GestãoTransportes {
 
  
     public static void main(String[] args) {
+       
         int a = 0;
+        
         //LAYOUT DO MENU
         System.out.println("\nBEM-VINDO AO GESTOR DE TRANSPORTES:\n");
-        
         System.out.println("1. Inserir e consultar (através no NIF) todas as empresas registadas");
-        System.out.print("2. Inserir, consultar (através no NIF) e alterar dados de motoristas");
+        System.out.println("2. Inserir, consultar (através no NIF) e alterar dados de motoristas");
         System.out.println("3. Inserir, consultar (através na matrícula) veículos");
-        System.out.print("4. Associar veículo a nova empresa");
+        System.out.println("4. Associar veículo a nova empresa");
         System.out.println("5. Registar e consultar avarias");
-        System.out.print("6. Registar e consultar (por empresa) serviços de transporte registados");
+        System.out.println("6. Registar e consultar (por empresa) serviços de transporte registados");
         System.out.println("7. Outras consultas/estatísticas");
         System.out.print("---------------------------------------------------------------------");
                
-        //----------------------------------------------------------------
-        
-//        Scanner opcao = new Scanner(System.in);
-//        System.out.print("\n Indique a sua opção: ");
-//        int opc = opcao.nextInt();
         int opc = Consola.lerInt("\n Indique a sua opção: ", 1, 7);
         System.out.print("\n");
 
 
             switch(opc) {
                 case 1:
-                    
+                    System.out.println("opc1");
                 case 2:
-                   
+                   System.out.println("opc2");
                 case 3:
-                     
+                     System.out.println("opc3");
                 case 4:
-                     
+                     System.out.println("opc4");
                 case 5:
-                     
+                     System.out.println("opc5");
                 case 6:
-                     
+                     System.out.println("opc6");
                 case 7:
+                    System.out.println("opc7");
                     a = 1;
                 default:
                     main(args);
@@ -51,20 +48,16 @@ public class GestãoTransportes {
     
     if( a == 1){
         //LAYOUT DO SUB-MENU
-        System.out.println("\nSUB-MENU:\n");    
-    
+        System.out.println("\nSUB-MENU:\n");   
         System.out.println("1. Listagem de veículos com mais de n quilómetros realizados");
-        System.out.print("2. Listagem das 3 empresas com maior valor faturado");
+        System.out.println("2. Listagem das 3 empresas com maior valor faturado");
         System.out.println("3. Total de serviços realizados por empresa");
-        System.out.print("4. Média de quilómetros realizados por empresa e por tipo de serviço");
+        System.out.println("4. Média de quilómetros realizados por empresa e por tipo de serviço");
         System.out.println("5. Listagem de funcionários (por empresa)");
-        System.out.print("6. Histórico de avarias por veículo.");
+        System.out.println("6. Histórico de avarias por veículo.");
         System.out.println("7. Voltar ao Menu Principal");
         System.out.print("---------------------------------------------------------------------");    
     
-//         Scanner opcao2 = new Scanner(System.in);
-//        System.out.print("\n Indique a sua opção: ");
-//        int opc2 = opcao2.nextInt();
        int opc2 = Consola.lerInt("\n Indique a sua opção: ", 1, 7);
         System.out.print("\n");       
     
@@ -86,10 +79,10 @@ public class GestãoTransportes {
                                      
                 default:
                     main(args);
-                                    }
-}          
+            }
+        }     
+    }
   //--------------------------------------------------------------------------      
     
-    }
     
 }
