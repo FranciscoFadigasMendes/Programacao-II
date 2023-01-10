96136b589ae8a29ab49fb286e9581c7965ced798
FranciscoFadigasMendes <79844404+FranciscoFadigasMendes@users.noreply.github.com>
2023-01-10 20:11:19 +0000

    Update Empresa.java

--- PROJETO/Empresa.java
+++ PROJETO/Empresa.java
@@ -1,33 +1,49 @@
 package com.mycompany.gestaotransportes;
-
-
+import java.io.Serializable;
+import util.Ficheiro;
 
 /**
  *
- * @author franc
+ * @author Francisco Mendes e Francisco Chen
  */
-
-    public class Empresa {
+public class Empresa implements Serializable {
         
     //ATRIBUTOS
-    private int NIF;
-    private String nome;
-    private String  morada;
+    private int NIF,Job_Counter;
+    private String nome,morada;
+//    private float fatura;
     
     
     
     //CONSTRUTOR
-    public  Empresa(int NIF, String nome, String  morada){
+
+    /**
+     *
+     * @param NIF
+     * @param nome
+     * @param morada
+     * @param Job_Counter
+     */
+    public  Empresa(int NIF, String nome, String  morada, int Job_Counter){
         
         this.NIF = NIF;
         this.nome = nome;
         this.morada = morada;
+//        this.fatura = fatura;
+        this.Job_Counter = Job_Counter;
     }
+    
+    /**
+     * Método para mostrar os atributos todos de uma vez
+     */
     public void mostrarDados(){
         System.out.println("NIF: "+getNIF());
         System.out.println("Nome: "+getNome());
         System.out.println("Morada: "+getMorada());
+        System.out.println("Contador de Serviços: "+getJob_Counter());
+
     }
+    
 
     /**
      * @return the NIF
@@ -70,5 +86,29 @@ package com.mycompany.gestaotransportes;
     public void setMorada(String morada) {
         this.morada = morada;
     }
+
+//    /**
+//     * @return the fatura
+//     */
+//    public float getFatura() {
+//        return fatura;
+//    }Job_Counter
+    
+    /**
+     * @return Job_Counter
+     */
+    public int getJob_Counter() {
+        return Job_Counter;
+    }
+    
+    /**
+     * @param Job_Counter 
+     */
+    public void Job_Counter(int Job_Counter) {
+        this.Job_Counter = Job_Counter;
+    }    
+    
+    
     
    }
+
