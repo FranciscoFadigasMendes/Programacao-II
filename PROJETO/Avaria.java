57c381be508b484f537ebdff8d16104cfd35a7d9
FranciscoFadigasMendes <79844404+FranciscoFadigasMendes@users.noreply.github.com>
2023-01-10 20:10:58 +0000

    Update Avaria.java

--- PROJETO/Avaria.java
+++ PROJETO/Avaria.java
@@ -1,5 +1,63 @@
-package gestãotransportes;
+package com.mycompany.gestaotransportes;
+import util.Ficheiro;
+import java.io.Serializable;
 
-public class Avaria {
+/**
+ *
+ * @author Francisco Mendes e Francisco Chen
+ */
+public class Avaria extends Veiculo implements Serializable {
+    
+    //ATRIBUTOS
+    protected String descrição_danos;
+    protected float custo;
+    protected long periodo_parado;
+    //CONSTRUTOR
+     
+    /**
+     *
+     * @param v
+     * @param descrição_danos
+     * @param custo
+     * @param periodo_parado
+     */
+    public Avaria(Veiculo v,String descrição_danos,float custo,long periodo_parado){         
+        super(v.marca,v.modelo,v.matricula,v.AnoRegisto,v.quilometragem,v.cilindrada,v.tipo,v.capacidade,v.empresa,v.avaria,v.Disponibilidade);
+        this.descrição_danos = descrição_danos;
+        this.custo = custo;
+        this.periodo_parado = periodo_parado;
+    }
+     
+    /**
+     * Método para mostrar os atributos todos de uma vez
+     */
+    @Override
+    public void mostrarDados(){
+        System.out.println("Descrição dos Danos: "+getDescrição_danos()); 
+        System.out.println("Custo: "+getCusto()); 
+        System.out.println("Período Parado (em dias): "+getPeriodo_parado()); 
+    }
+        
+        
+    /**
+     * @return descrição_danos
+     */
+    public String getDescrição_danos() {
+        return descrição_danos;
+    }
+    
+    /**
+     * @return custo
+     */
+    public float getCusto() {
+        return custo;
+    }
+    
+    /**
+     * @return periodo_parado
+     */
+    public long getPeriodo_parado() {
+        return periodo_parado;
+    }   
     
 }
